8df37b193993ca77d90d009ad369609ba0d33d2f
Djordje Lukic <djordje.lukic@docker.com>
2021-02-07 16:13:59 +0100

    Eval prefix bang operator

--- eval/eval.go
+++ eval/eval.go
@@ -21,6 +21,9 @@ func Eval(node ast.Node) object.Object {
 		return &object.Integer{Value: node.Value}
 	case *ast.Boolean:
 		return nativeBoolToBooleanObject(node.Value)
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPrefixExpression(node.Operator, right)
 	}
 
 	return nil
@@ -42,4 +45,26 @@ func evalStatements(statements []ast.Statement) object.Object {
 	}
 
 	return result
+}
+
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+	switch operator {
+	case "!":
+		return evalBangOperatorExpression(right)
+	default:
+		return Null
+	}
+}
+
+func evalBangOperatorExpression(right object.Object) object.Object {
+	switch right {
+	case True:
+		return False
+	case False:
+		return True
+	case Null:
+		return True
+	default:
+		return False
+	}
 }
--- eval/eval_test.go
+++ eval/eval_test.go
@@ -23,6 +23,25 @@ func TestEvalIntegerExperssion(t *testing.T) {
 	}
 }
 
+func TestBangOperator(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{"!true", false},
+		{"!false", true},
+		{"!5", false},
+		{"!!true", true},
+		{"!!false", false},
+		{"!!5", true},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testBooleanObject(t, evaluated, tt.expected)
+	}
+}
+
 func testEval(input string) object.Object {
 	l := lexer.New(input)
 	p := parser.New(l)
@@ -40,6 +59,21 @@ func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
 
 	if result.Value != expected {
 		t.Fatalf("object has wrong value, got %d, want %d", result.Value, expected)
+		return false
+	}
+
+	return true
+}
+
+func testBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
+	result, ok := obj.(*object.Boolean)
+	if !ok {
+		t.Fatalf("object is not Boolean, got %t", obj)
+		return false
+	}
+
+	if result.Value != expected {
+		t.Fatalf("object has wrong value, got %t, want %t", result.Value, expected)
 		return false
 	}
 
