d6fd8c1ef3cf47cfeb893eef834408fcef0d64ef
Djordje Lukic <djordje.lukic@docker.com>
2021-02-07 14:46:22 +0100

    repl prints back the source code

--- repl/repl.go
+++ repl/repl.go
@@ -6,6 +6,7 @@ import (
 	"io"
 
 	"github.com/rumpl/monkey-lang/lexer"
+	"github.com/rumpl/monkey-lang/parser"
 	"github.com/rumpl/monkey-lang/token"
 )
 
@@ -23,9 +24,24 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
+
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
+		}
+
+		fmt.Fprintln(out, program.String())
 
 		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
 			fmt.Fprintf(out, "%+v\n", tok)
 		}
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		fmt.Fprintln(out, "\t"+msg)
 	}
 }
